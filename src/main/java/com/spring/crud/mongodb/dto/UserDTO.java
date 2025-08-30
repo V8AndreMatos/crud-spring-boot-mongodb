5eb3dfe1f23eb0e5d6cde26710ab960e0185ef92
Andre <andreluisv8@gmail.com>
2025-08-30 19:21:00 -0300

    Added class : UserDTO

---
+++ src/main/java/com/spring/crud/mongodb/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package com.spring.crud.mongodb.dto;
+
+import com.spring.crud.mongodb.entity.UserEntity;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserDTO(){
+
+    }
+
+    public UserDTO(UserEntity entity){
+
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/spring/crud/mongodb/service/UserService.java
+++ src/main/java/com/spring/crud/mongodb/service/UserService.java
@@ -1,4 +1,46 @@
 package com.spring.crud.mongodb.service;
 
+import com.spring.crud.mongodb.dto.UserDTO;
+import com.spring.crud.mongodb.entity.UserEntity;
+import com.spring.crud.mongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> findAllUsers(){
+
+        return userRepository.findAll().stream().map( x -> new UserDTO(x)).toList();
+    }
+
+    public Optional<UserDTO> findUserById(Long id) {
+
+        return userRepository.findById(id).stream().map(x -> new UserDTO(x)).toList();
+    }
+
+    public UserDTO createUser(UserDTO userDTO){
+
+        UserEntity userEntity = new UserEntity(null,userDTO.getName(), userDTO.getEmail());
+        return new UserDTO(userRepository.save(userEntity));
+    }
+
+    public Optional<UserDTO> update(Long id, UserDTO dto) {
+        return userRepository.findById(id).map(existing -> {
+            existing.setName(dto.getName());
+            existing.setEmail(dto.getEmail());
+            return new UserDTO(userRepository.save(existing));
+        });
+    }
+
+    public void deleteById(Long id){
+        userRepository.findById(id);
+    }
+
 }
